552b2514b96201ca6c3ee635e7645ab43b57fdaf
SmitasCodes <airidassmith30@gmail.com>
2026-02-23 14:13:24 +0200

    added first proper routes for users

--- index.ts
+++ index.ts
@@ -2,18 +2,14 @@ import "dotenv/config";
 import "./config/env.ts";
 import cors from "cors";
 import express from "express";
-//testing
-import { createUser } from "./controllers/usersController.ts";
-//
+import usersRoutes from "./routes/usersRoutes.ts";
 
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// testing
-app.post("/api/users", createUser);
-//
+app.use("/api/users", usersRoutes);
 
 app.listen(port, () => {
   console.log(`Server is now running on port: ${port}`);
