25b01ad7e7d8bac8515a6783a3eaefacacde90a7
SmitasCodes <airidassmith30@gmail.com>
2026-02-26 15:24:13 +0200

    added get sessions api

--- controllers/sessionsController.ts
+++ controllers/sessionsController.ts
@@ -18,4 +18,62 @@ const createSession = asyncHandler(async (req: Request, res: Response) => {
     .json({ id: session.id, templateId, userId, createdAt: session.createdAt });
 });
 
-export { createSession };
+const getSessions = asyncHandler(async (req: Request, res: Response) => {
+  const userId = req.user.id;
+
+  if (!userId) {
+    res.status(400);
+    throw new Error("User id is missing");
+  }
+
+  const sessions = await prisma.session.findMany({
+    where: { userId },
+    omit: { userId: true },
+    include: {
+      template: { select: { id: true, name: true } },
+      sets: {
+        select: {
+          id: true,
+          reps: true,
+          setNumber: true,
+          weight: true,
+          exercise: { select: { id: true, name: true } },
+        },
+      },
+    },
+  });
+
+  res.status(200).json({
+    sessions,
+  });
+});
+
+const getSession = asyncHandler(async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const userId = req.user.id;
+
+  if (!id || !userId) {
+    res.status(400);
+    throw new Error("Missing fields for session");
+  }
+
+  const session = await prisma.session.findUnique({
+    where: { id, userId },
+    include: {
+      template: { select: { id: true, name: true } },
+      sets: {
+        select: {
+          id: true,
+          reps: true,
+          setNumber: true,
+          weight: true,
+          exercise: { select: { id: true, name: true } },
+        },
+      },
+    },
+  });
+
+  res.status(200).json({ session });
+});
+
+export { createSession, getSessions, getSession };
--- routes/sessionsRoutes.ts
+++ routes/sessionsRoutes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { protect } from "../middleware/authMiddleware.ts";
-import { createSession } from "../controllers/sessionsController.ts";
+import {
+  createSession,
+  getSessions,
+  getSession
+} from "../controllers/sessionsController.ts";
 import { createSet } from "../controllers/setsController.ts";
 
 const router = Router();
 
-router.post("/", protect, createSession);
-router.post("/:sessionId/sets/", protect, createSet);
+router.post("/", protect, createSession).get("/", protect, getSessions);
+router.get("/:id", protect, getSession);
+router.post("/:id/sets/", protect, createSet);
 
 export default router;
